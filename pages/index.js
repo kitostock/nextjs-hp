7b33d8232409bd90b7d406a9eec8697d8fe9429f
kitostock <ito0405r.s@gmail.com>
2022-03-02 00:25:54 +0900

    change index

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ export default function Home() {
   return (
     <Layout title="Home">
       <p className="text-4xl">Welcome to Nextjs!!!!!!update</p>
-      <p className="text-4xl">not change!!!!!!update</p>
     </Layout>
   );
 }
